384eaa2500bc9c891a87d909a29e60763c5d6dcd
Jiří Kofroň <kofron.jiri@gmail.com>
2023-11-07 22:41:30 +0100

    add eslint, basic raindrops animation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
     '@hypernym/nuxt-gsap',
     '@vueuse/nuxt',
   ]
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,49 @@
 <script setup lang="ts">
-import { useWindowSize } from '@vueuse/core'
+import { useElementSize } from '@vueuse/core'
 
-const raindrops = ref(null)
+const canvasWrapper = ref()
+const raindrops = ref()
 
-const { width, height } = useWindowSize()
+const { width, height } = useElementSize(canvasWrapper)
 
 onMounted(() => {
-  setInterval(createRaindrop, 600)
+  const ctx = (raindrops.value as HTMLCanvasElement).getContext('2d') as CanvasRenderingContext2D
+  setInterval(createRaindrop, 200, ctx)
 })
 
-const createRaindrop = () => {
-  if(raindrops.value) {
-    const ctx = (raindrops.value as HTMLCanvasElement).getContext("2d") as CanvasRenderingContext2D;
-      ctx.beginPath()
-      let x = Math.floor(Math.random() * (width.value / 2))
-      let y = Math.floor(Math.random() * (height.value / 2))
-      let radius = Math.floor(Math.random() * 10)
+const createRaindrop = (ctx: CanvasRenderingContext2D) => {
+  ctx.beginPath()
+  const x = Math.floor(Math.random() * width.value)
+  const y = Math.floor(Math.random() * height.value)
+  const radius = Math.floor(Math.random() * (11 - 3) + 3)
 
-      console.log(x, y)
+  ctx.filter = 'contrast(1.7) brightness(1.7) saturate(1.5) drop-shadow(0 2px 1px white)'
+  ctx.fillStyle = 'transparent'
 
-      ctx.arc(x, y, radius, 0, 360)
-      ctx.fill()
-  }
+  ctx.arc(x, y, radius, 0, 1.5 * Math.PI)
+  ctx.closePath()
+
+  ctx.fill()
+
+  const img = new Image()
+  img.classList.add('rain')
+  img.src = '/img/midnight_rain3.png'
+
+  img.addEventListener('load', function () {
+    ctx.setTransform(1, 0, 0, 1, x, y)
+    ctx.rotate(Math.floor(Math.random() * 360) * Math.PI / 180)
+    ctx.drawImage(img, x - 2, y - 2, radius - 1, radius - 1)
+    ctx.save()
+  }, true)
 }
 
 </script>
 
 <template>
   <main class="relative w-full h-screen">
-    <div class="absolute left-0 top-0 w-full h-screen landing bg-origin-border bg-center bg-no-repeat bg-cover blur-sm z-1" >
-      <canvas ref="raindrops" class="w-full h-screen"></canvas>
+    <div ref="canvasWrapper" class="absolute left-0 top-0 w-full h-screen landing bg-origin-border bg-center bg-no-repeat bg-cover blur-sm z-1" />
+    <div ref="canvasWrapper" class="absolute left-0 top-0 w-full h-screen">
+      <canvas ref="raindrops" :width="width" :height="height" class="h-screen" />
     </div>
     <section class="absolute left-0 top-0 w-full h-screen z-100 flex items-center justify-center">
       BOHREN
