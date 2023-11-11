fff8cdeecdecb5240b93d884de2c789e22e67221
Jiří Kofroň <kofron.jiri@gmail.com>
2023-11-12 00:49:30 +0100

    WIP mobile menu

---
+++ components/navigation/HamburgerMenu.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import data from '@/data/menuItem.json'
+import type { MenuItem } from '~/types/types'
+
+const { locale } = useI18n()
+const localePath = useLocalePath()
+
+const menuItems: MenuItem[] = data
+</script>
+
+<template>
+  <div class="flex flex-col z-30">
+    header menu
+    <nav class="flex flex-col items-center justify-center gap-y-4">
+      <NuxtLink
+        v-for="menuItem in menuItems"
+        :key="menuItem.id"
+        :to="localePath(menuItem.to, locale)"
+      >
+        {{ $t(menuItem.name) }}
+      </NuxtLink>
+    </nav>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- components/navigation/TheHeader.vue
+++ components/navigation/TheHeader.vue
@@ -1,68 +1,47 @@
 <script setup lang="ts">
+import data from '@/data/menuItem.json'
+import type { MenuItem } from '~/types/types'
+import HamburgerMenu from '~/components/navigation/HamburgerMenu.vue'
 const { locale } = useI18n()
 const localePath = useLocalePath()
 
-interface MenuItem {
-  id: number
-  name: string,
-  to: string
-}
-
-const menuItems: MenuItem[] = [
-  {
-    id: 1,
-    name: 'menu.news',
-    to: ''
-  },
-  {
-    id: 2,
-    name: 'menu.biography',
-    to: '/biography'
-  },
-  {
-    id: 3,
-    name: 'menu.discography',
-    to: '/discography'
-  },
-  {
-    id: 4,
-    name: 'menu.videos',
-    to: '/media'
-  },
-  {
-    id: 5,
-    name: 'menu.contact',
-    to: '/contact'
-  }
-]
+const menuItems: MenuItem[] = data
 </script>
 
 <template>
-  <nav class="fixed top-0 left-0 right-0 z-50 flex items-center justify-between bg-default-red font-heading p-6">
-    <div class="flex flex-col justify-center">
-      <div class="text-5xl">
-        BOHREN
+  <div class="fixed top-0 left-0 right-0 z-50 flex flex-col items-center ">
+    <nav class="w-full flex items-center justify-between bg-default-red font-heading p-6">
+      <div class="flex flex-col justify-center">
+        <div class="text-5xl">
+          BOHREN
+        </div>
+        <div class="text-base">
+          & DER CLUB OF GORE
+        </div>
       </div>
-      <div class="text-base">
-        & DER CLUB OF GORE
+      <div class="flex flex-auto items-center justify-around max-w-lg">
+        <NuxtLink
+          v-for="menuItem in menuItems"
+          :key="menuItem.id"
+          :to="localePath(menuItem.to, locale)"
+        >
+          {{ $t(menuItem.name) }}
+        </NuxtLink>
       </div>
-    </div>
-    <div class="flex flex-auto items-center justify-around max-w-lg">
-      <NuxtLink
-        v-for="menuItem in menuItems"
-        :key="menuItem.id"
-        :to="localePath(menuItem.to, locale)"
-      >
-        {{ $t(menuItem.name) }}
-      </NuxtLink>
-    </div>
 
-    <LangSwitch />
+      <LangSwitch />
 
-    <div>
-      merch
-    </div>
-  </nav>
+      <div>
+        merch
+      </div>
+
+      <div>
+        Menu
+      </div>
+    </nav>
+
+    <HamburgerMenu />
+  </div>
 </template>
 
 <style scoped lang="scss">
